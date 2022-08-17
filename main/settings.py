b20be485fe519247eec98e5c33bf559d01a61242
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-08-17 20:38:00 +0000

    Add lint formatter and django extensions to dependencies

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third party
+    "django_extensions",
     # Local
     "courses.apps.CoursesConfig",
 ]
