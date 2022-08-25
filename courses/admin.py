351cdd8b38e146c66dda6653bb08bf02e03c81b8
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-08-25 14:33:02 +0200

    Add Course Admin with an inline ModuleAdmin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Content, Course, File, Image, Module, Subject, Text, Video
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug"]
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["title", "subject", "created"]
+    list_filter = ["created", "subject"]
+    search_fields = ["title", "overview"]
+    prepopulated_fields = {"slug": ("title",)}
+    inlines = [ModuleInline]
+
+
+admin.site.register(Content)
+admin.site.register(Text)
+admin.site.register(Image)
+admin.site.register(Video)
+admin.site.register(File)
