5b219d63dfe90c01853a528c4a96a814b342bc90
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-09-04 18:12:55 -0300

    URL conf for the students app

---
+++ students/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = "students"
+
+urlpatterns = [
+    path(
+        "register/",
+        views.StudentRegistrationView.as_view(),
+        name="student_registration",
+    ),
+]
