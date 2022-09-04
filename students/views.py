af10cf3eba976d82c0b96650a4c02e3e43201ed6
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-09-04 18:12:43 -0300

    Implement Student Registration view

--- students/views.py
+++ students/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+
+class StudentRegistrationView(CreateView):
+    template_name = "students/registration.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("students:student_course_list")
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        cd = form.cleaned_data
+        user = authenticate(username=cd["username"], password=cd["password1"])
+        login(self.request, user)
+        return result
