74115bb235ee4670281dbf56329f52e0ae118974
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-09-05 17:22:10 -0300

    Add REDIRECT_LOGIN_URL to student_course_list

--- main/settings.py
+++ main/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -165,3 +167,5 @@ LOGGING = {
         },
     },
 }
+
+LOGIN_REDIRECT_URL = reverse_lazy("students:student_course_list")
