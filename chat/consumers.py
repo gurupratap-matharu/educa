67581b1cd4bb9d68cb2da0acad6883557944abf6
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-09-11 16:19:38 -0300

    A Websocket consumer which for the chat app

---
+++ chat/consumers.py
@@ -0,0 +1,29 @@
+"""
+Consumers are the equivalent of Django views for asynchronous applications.
+They handle WebSockets in a very similar way to how traditional django views handle
+HTTP requests.
+
+Consumers are ASGI applications that can handle messages, notifications and are built
+for long-running communication.
+
+URLs are mapped to consumers through routing classes that allow us to combine and
+stack consumers."""
+
+import json
+
+from channels.generic.websocket import WebsocketConsumer
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, code):
+        pass
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # send message to websocket
+        self.send(text_data=json.dumps({"message": message}))
