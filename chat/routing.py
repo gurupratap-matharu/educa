3e76256364f62a20f84b6fde62e2899b7cf92c4b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-09-17 14:00:02 -0300

    Ignore warning by pylance

--- chat/routing.py
+++ chat/routing.py
@@ -10,5 +10,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/room/(?P<course_id>\d+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/room/(?P<course_id>\d+)/$", consumers.ChatConsumer.as_asgi()),  # type: ignore
 ]
