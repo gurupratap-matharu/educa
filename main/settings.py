89b276fa268d96b3c2a11f71a69e0925b8abc24c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-09-17 13:59:29 -0300

    Add channel_layers settings for channel-redis package

--- main/settings.py
+++ main/settings.py
@@ -189,6 +189,17 @@ CACHES = {
     }
 }
 
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
+
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
