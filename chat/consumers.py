d7f1d6db9906f3da32bfaa3b77031002faf2a0e8
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-09-18 10:54:48 -0300

    Add timestamp and user for each message

--- chat/consumers.py
+++ chat/consumers.py
@@ -11,19 +11,59 @@ stack consumers."""
 
 import json
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope["user"]
+        self.id = self.scope["url_route"]["kwargs"]["course_id"]
+        self.room_group_name = "chat_%s" % self.id
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
+        # Accept connection
         self.accept()
 
     def disconnect(self, code):
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
     def receive(self, text_data):
         data_dict = json.loads(text_data)
         message = data_dict["message"]
+        now = timezone.now()
 
-        # send message to websocket
-        self.send(text_data=json.dumps({"message": message}))
+        # send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+                "user": self.user.username,
+                "datetime": now.isoformat(),
+            },
+        )
+
+    def chat_message(self, event):
+        """
+        Receive message from the room group.
+
+        This method is named as chat_message() to match the `type` key that is sent to
+        the channel group when a message is received from the WebSocket.
+
+        This way when a message with type `chat_message` is sent to the group, all
+        consumers subscribed to the group will receive the message and will execute the
+        `chat_message()` method.
+
+        And we basically send the event message received to the WebSocket.
+        """
+
+        self.send(text_data=json.dumps(event))
