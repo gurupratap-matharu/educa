865f23fcbc9005210ae2b563dd416c1004786bb1
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-09-25 11:48:35 -0300

    Using channels asgi app as the default app

--- main/asgi.py
+++ main/asgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+django.setup()
 
-application = get_asgi_application()
+"""
+Load the default ASGI application using channels instead of the standard Django ASGI
+module. This is because we wish to use Daphne with protocol servers.
+
+Read more here: https://channels.readthedocs.io/en/latest/deploying.html#run-protocol-servers.
+"""
+
+application = get_default_application()
