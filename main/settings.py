4abe2c97e387226ff2dba05344236b27827481c0
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-09-27 11:51:16 -0300

    Add subdomain course middleware to Middleware list

--- main/settings.py
+++ main/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "courses.middleware.subdomain_course_middleware",
 ]
 
 ROOT_URLCONF = "main.urls"
