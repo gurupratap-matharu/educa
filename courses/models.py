a4dcdfd38fc00eccce3776feb7c3e3859d4db390
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-10-02 13:18:14 -0300

    User the settings.AUTH_USER_MODEL in foreign key references

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
