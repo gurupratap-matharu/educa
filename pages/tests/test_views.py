f9987188baf6b03e8c401bfb75e8472e2035e469
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-10-03 16:22:11 -0300

    Unit tests for HomePageView

--- pages/tests/test_views.py
+++ pages/tests/test_views.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
 
-# Create your tests here.
+from pages.views import HomePageView
+
+
+class HomePageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("pages:home")
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, "pages/home.html")
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "Home")
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "Hi there! I should not be on this page.")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve(reverse("pages:home"))
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
