b92d10234d3a117a096d102d6a82427b1a3bb37c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-10-04 18:29:04 -0300

    Remove verbose access log format

--- config/prod/gunicorn.conf.py
+++ config/prod/gunicorn.conf.py
@@ -18,7 +18,6 @@ workers = multiprocessing.cpu_count() * 2 + 1
 
 # Access log - records incoming HTTP requests
 accesslog = "-"
-access_log_format = "{'remote_ip':'%({X-Real-IP}i)s','request_id':'%({X-Request-Id}i)s','response_code':'%(s)s','request_method':'%(m)s','request_path':'%(U)s','request_querystring':'%(q)s','request_timetaken':'%(D)s','response_length':'%(B)s'}"
 
 # Error log - records Gunicorn server errors
 errorlog = "-"
