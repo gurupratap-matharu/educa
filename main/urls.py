4dac1238e8f500e48b4c5a1a1a16a187b26abbb3
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-10-05 16:41:17 -0300

    Hook up django-allauth urls

--- main/urls.py
+++ main/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     # path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
     # Local apps
     path("accounts/", include("users.urls"), name="users"),
     path("courses/", include("courses.urls"), name="courses"),
