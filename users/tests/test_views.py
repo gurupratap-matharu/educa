2a40944221f94e82179bee23001ee4c5a1eb28ea
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-10-05 17:06:31 -0300

    Remove redundant test of custom creation form

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
 
-from users import forms, views
+from users import views
 
 
 class SignupPageTests(TestCase):
@@ -11,7 +11,7 @@ class SignupPageTests(TestCase):
 
     def test_signup_template(self):
         self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, "registration/signup.html")
+        self.assertTemplateUsed(self.response, "account/signup.html")
         self.assertContains(self.response, "Signup")
         self.assertNotContains(self.response, "Hi I should not be on this page!")
 
@@ -21,8 +21,3 @@ class SignupPageTests(TestCase):
         expected_view = views.SignupPageView.as_view().__name__
 
         self.assertEqual(actual_view, expected_view)
-
-    def test_signuppage_view_contains_correct_form(self):
-        form = self.response.context.get("form")
-        self.assertIsInstance(form, forms.CustomUserCreationForm)
-        self.assertContains(self.response, "csrfmiddlewaretoken")
