3c6796d9c8e2b53f121e6f3389701d2e37db1438
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-10-09 10:46:29 -0300

    Remove unused imports

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
+from . import views
+
 app_name = "users"
 
-
-from . import views
 
 urlpatterns = [
     path("signup/", views.SignupPageView.as_view(), name="signup"),
