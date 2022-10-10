5ce3fef415a6917bd70a01c01cb1228dffee2d87
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-10-10 13:05:48 -0300

    Replace 200 code with HTTPStatus.OK

--- pages/tests/test_views.py
+++ pages/tests/test_views.py
@@ -1,6 +1,9 @@
+from http import HTTPStatus
+
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
 
+from pages.forms import ContactForm
 from pages.views import (
     AboutPageView,
     ContactPageView,
@@ -8,18 +11,19 @@ from pages.views import (
     PrivacyPageView,
     TermsPageView,
 )
-from pages.forms import ContactForm
+
 
 class HomePageTests(SimpleTestCase):
     def setUp(self):
-        url = reverse("pages:home")
-        self.response = self.client.get(url)
+        self.url = reverse("pages:home")
+        self.response = self.client.get(self.url)
+        self.template_name = "pages/home.html"
 
     def test_homepage_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
 
     def test_homepage_template(self):
-        self.assertTemplateUsed(self.response, "pages/home.html")
+        self.assertTemplateUsed(self.response, self.template_name)
 
     def test_homepage_contains_correct_html(self):
         self.assertContains(self.response, "Home")
@@ -28,20 +32,21 @@ class HomePageTests(SimpleTestCase):
         self.assertNotContains(self.response, "Hi there! I should not be on this page.")
 
     def test_homepage_url_resolves_homepageview(self):
-        view = resolve(reverse("pages:home"))
+        view = resolve(self.url)
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
 
 
 class AboutPageTests(SimpleTestCase):
     def setUp(self):
-        url = reverse("pages:about")
-        self.response = self.client.get(url)
+        self.url = reverse("pages:about")
+        self.response = self.client.get(self.url)
+        self.template_name = "pages/about.html"
 
     def test_aboutpage_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
 
     def test_aboutpage_renders_correct_template(self):
-        self.assertTemplateUsed(self.response, "pages/about.html")
+        self.assertTemplateUsed(self.response, self.template_name)
 
     def test_aboutpage_contains_correct_html(self):
         self.assertContains(self.response, "About")
@@ -50,20 +55,21 @@ class AboutPageTests(SimpleTestCase):
         self.assertNotContains(self.response, "Hi there! I should not be on this page.")
 
     def test_aboutpage_url_resolves_aboutpageview(self):
-        view = resolve(reverse("pages:about"))
+        view = resolve(self.url)
         self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
 
 
 class TermsPageTests(SimpleTestCase):
     def setUp(self):
-        url = reverse("pages:terms")
-        self.response = self.client.get(url)
+        self.url = reverse("pages:terms")
+        self.response = self.client.get(self.url)
+        self.template_name = "pages/terms.html"
 
     def test_termspage_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
 
     def test_termspage_renders_correct_template(self):
-        self.assertTemplateUsed(self.response, "pages/terms.html")
+        self.assertTemplateUsed(self.response, self.template_name)
 
     def test_termspage_contains_correct_html(self):
         self.assertContains(self.response, "Terms")
@@ -72,20 +78,21 @@ class TermsPageTests(SimpleTestCase):
         self.assertNotContains(self.response, "Hi I should not be on this page!")
 
     def test_termspage_url_resolves_termspageview(self):
-        view = resolve(reverse("pages:terms"))
+        view = resolve(self.url)
         self.assertEqual(view.func.__name__, TermsPageView.as_view().__name__)
 
 
 class PrivacyPageTests(SimpleTestCase):
     def setUp(self):
-        url = reverse("pages:privacy")
-        self.response = self.client.get(url)
+        self.url = reverse("pages:privacy")
+        self.response = self.client.get(self.url)
+        self.template_name = "pages/privacy.html"
 
     def test_privacy_page_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
 
     def test_privacy_page_renders_correct_template(self):
-        self.assertTemplateUsed(self.response, "pages/privacy.html")
+        self.assertTemplateUsed(self.response, self.template_name)
 
     def test_privacy_page_contains_correct_html(self):
         self.assertContains(self.response, "Privacy")
@@ -94,17 +101,18 @@ class PrivacyPageTests(SimpleTestCase):
         self.assertNotContains(self.response, "Hi I should not be on this page!")
 
     def test_privacy_page_url_resolves_privacypageview(self):
-        view = resolve(reverse("pages:privacy"))
+        view = resolve(self.url)
         self.assertEqual(view.func.__name__, PrivacyPageView.as_view().__name__)
 
 
 class ContactPageTests(SimpleTestCase):
     def setUp(self):
-        url = reverse("pages:contact")
-        self.response = self.client.get(url)
+        self.url = reverse("pages:contact")
+        self.response = self.client.get(self.url)
+        self.field_required_msg = "This field is required."
 
     def test_contact_page_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
 
     def test_contact_page_renders_correct_template(self):
         self.assertTemplateUsed(self.response, "pages/contact.html")
@@ -116,9 +124,50 @@ class ContactPageTests(SimpleTestCase):
         self.assertNotContains(self.response, "Hi I should not be on this page!")
 
     def test_contact_page_url_resolves_contactpageview(self):
-        view = resolve(reverse("pages:contact"))
+        view = resolve(self.url)
         self.assertEqual(view.func.__name__, ContactPageView.as_view().__name__)
 
     def test_contact_page_renders_contactform(self):
-        form = self.response.context['form']
+        form = self.response.context["form"]
         self.assertIsInstance(form, ContactForm)
+
+    def test_contact_page_with_empty_form_submission_raises_valid_errors(self):
+        response = self.client.post(self.url, data={})
+        self.assertFormError(response, "form", "name", self.field_required_msg)
+        self.assertFormError(response, "form", "email", self.field_required_msg)
+        self.assertFormError(response, "form", "subject", self.field_required_msg)
+        self.assertFormError(response, "form", "message", self.field_required_msg)
+
+    def test_contact_page_with_invalid_field_lengths_raises_valid_errors(self):
+        invalid_data = {
+            "name": "ab",
+            "email": "a@b.com",
+            "subject": "XX",
+            "message": "-",
+        }
+        response = self.client.post(self.url, invalid_data)
+
+        self.assertFormError(
+            response,
+            "form",
+            "name",
+            "Ensure this value has at least 3 characters (it has 2).",
+        )
+        self.assertFormError(
+            response,
+            "form",
+            "email",
+            "Ensure this value has at least 10 characters (it has 7).",
+        )
+        self.assertFormError(
+            response,
+            "form",
+            "subject",
+            "Ensure this value has at least 3 characters (it has 2).",
+        )
+        self.assertFormError(
+            response,
+            "form",
+            "message",
+            "Ensure this value has at least 20 characters (it has 1).",
+        )
--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import resolve, reverse
@@ -15,7 +17,7 @@ class SignupPageTests(TestCase):
         self.response = self.client.get(url)
 
     def test_signup_template(self):
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(self.response, "account/signup.html")
         self.assertContains(self.response, "Signup")
         self.assertNotContains(self.response, "Hi I should not be on this page!")
