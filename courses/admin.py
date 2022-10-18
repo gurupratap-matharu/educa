97af3d96c2c454f306a87e26a4edf3832dc71a48
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-10-17 22:12:07 -0300

    Improve interface for content items

--- courses/admin.py
+++ courses/admin.py
@@ -1,13 +1,9 @@
+from typing import Sequence
+
 from django.contrib import admin
 from embed_video.admin import AdminVideoMixin
 
 from .models import Content, Course, File, Image, Module, Subject, Text, Video
-
-
-@admin.register(Subject)
-class SubjectAdmin(admin.ModelAdmin):
-    list_display = ["title", "slug"]
-    prepopulated_fields = {"slug": ("title",)}
 
 
 class ModuleInline(admin.StackedInline):
@@ -18,28 +14,58 @@ class ContentInline(admin.StackedInline):
     model = Content
 
 
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display: Sequence[str] = ["title", "slug"]
+    prepopulated_fields = {"slug": ("title",)}
+
+
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ["title", "subject", "created"]
-    list_filter = ["created", "subject"]
-    search_fields = ["title", "overview"]
+    list_display: Sequence[str] = ["title", "subject", "created"]
+    list_filter: Sequence[str] = ["created", "subject"]
+    search_fields: Sequence[str] = ["title", "overview"]
     prepopulated_fields = {"slug": ("title",)}
     inlines = [ModuleInline]
 
 
 @admin.register(Module)
 class ModuleAdmin(admin.ModelAdmin):
-    list_display = ["title", "description", "order", "course"]
-    list_filter = ["course"]
-    search_fields = ["title", "description"]
+    list_display: Sequence[str] = ["title", "description", "order", "course"]
+    list_filter: Sequence[str] = ["course"]
+    search_fields: Sequence[str] = ["title", "description"]
     inlines = [ContentInline]
 
 
-admin.site.register(Text)
-admin.site.register(File)
-admin.site.register(Image)
+@admin.register(Text)
+class TextAdmin(admin.ModelAdmin):
+    list_display: Sequence[str] = ("title", "render", "owner", "created")
+    list_filter: Sequence[str] = (
+        "created",
+        "updated",
+    )
+    search_fields: Sequence[str] = ["title"]
 
 
 @admin.register(Video)
 class VideoAdmin(AdminVideoMixin, admin.ModelAdmin):
-    pass
+    list_display: Sequence[str] = ("title", "render", "owner", "created")
+    list_filter: Sequence[str] = (
+        "created",
+        "updated",
+    )
+    search_fields: Sequence[str] = ["title"]
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display: Sequence[str] = ("title", "image", "render", "owner", "created")
+    list_filter: Sequence[str] = ("created", "updated")
+    search_fields: Sequence[str] = ["title"]
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ("title", "file", "render", "owner", "created")
+    list_filter = ("created", "updated")
+    search_fields: Sequence[str] = ["title"]
